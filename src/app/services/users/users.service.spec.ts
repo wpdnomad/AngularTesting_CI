39348b622b03f7d46d7196c302a6dfa87374306e
Bill D <wpdnomad@yahoo.com>
2021-02-12 11:52:31 -0800

    Change users.service.spec.ts to user UsersService in providers and inject usersService with TestBed.get

--- src/app/services/users/users.service.spec.ts
+++ src/app/services/users/users.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let usersService: UsersService; // Add this
 
-  it('should be created', () => {
-    const service: UsersService = TestBed.get(UsersService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UsersService]
+    });
+
+    usersService = TestBed.get(UsersService); // Add this
+  });
+
+  it('should be created', () => { // Remove inject()
+    expect(usersService).toBeTruthy();
   });
 });
